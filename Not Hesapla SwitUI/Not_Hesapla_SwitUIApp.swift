76f14b125b8609e25d190d768bacec078e590128
Doğukan Çatmakaş <dcatmakas@gmail.com>
2023-04-30 00:06:05 +0300

    Initial Commit

---
+++ Not Hesapla SwitUI/Not_Hesapla_SwitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Not_Hesapla_SwitUIApp.swift
+//  Not Hesapla SwitUI
+//
+//  Created by Doğukan Çatmakaş on 30.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Not_Hesapla_SwitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
