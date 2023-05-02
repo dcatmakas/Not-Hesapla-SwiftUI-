a726b1e699b241eb92b50f70d1566c4dfca55a3d
Doğukan Çatmakaş <dcatmakas@gmail.com>
2023-05-02 10:25:05 +0300

    Not Hesapla (SwiftUI) started

---
+++ Not Hesapla SwitUI/Home.swift
@@ -0,0 +1,62 @@
+//
+//  Home.swift
+//  Not Hesapla SwitUI
+//
+//  Created by Doğukan Çatmakaş on 30.04.2023.
+//
+
+import SwiftUI
+
+struct Home: View {
+    @State private var isPresented: Bool = false
+    @State private var text: String = ""
+    
+    var body: some View {
+        VStack {
+            Text("Hoş Geldin!")
+                .bold()
+                .font(Font.system(size: 50))
+                .padding(.vertical, 60)
+            
+            Spacer()
+            
+            HStack {
+                Text("Lütfen Aşağıdaki Kutuya Adını Gir.")
+                    .padding(.leading, 40)
+                    .padding(.vertical)
+                
+                Spacer()
+            }
+            
+            TextField("Adını Gir", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal, 40)
+            
+            Spacer()
+            
+            Button {
+                isPresented = true
+            } label: {
+                Text("Başlayalım")
+                    .padding(.vertical, 15)
+                    .padding(.horizontal, 30)
+                    .font(Font.system(size: 20))
+            }
+            .background(Color(.blue))
+            .foregroundColor(Color(.white))
+            .cornerRadius(16)
+            Spacer()
+        }
+        .fullScreenCover(isPresented: $isPresented, content: {
+            SecondPage()
+        })
+    }
+}
+
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+
---
+++ Not Hesapla SwitUI/SecondPage.swift
@@ -0,0 +1,20 @@
+//
+//  SecondPage.swift
+//  Not Hesapla SwitUI
+//
+//  Created by Doğukan Çatmakaş on 2.05.2023.
+//
+
+import SwiftUI
+
+struct SecondPage: View {
+    var body: some View {
+        Text("test")
+    }
+}
+
+struct SecondPage_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondPage()
+    }
+}
